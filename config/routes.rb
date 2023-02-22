1f4fede346408d3e6600a0ce619ddfe5be056d3b
Joshua Waiswa <jwaiswa7@gmail.com>
2023-02-22 12:33:48 +0300

    Adding the landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: %i[new create]
   resources :companies
   resources :bookings
-  devise_for :admin_users
+  # devise_for :admin_users
   devise_for :users
   root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
